ca0705536f5735de566cb976a6c00ec0e3e5f8a2
Vinícius Lima Carneiro <viniciuslimacarneiro2006@gmail.com>
2026-08-26 19:12:10 -0300

    linter sucesso teste

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,17 @@
-API_KEY="DASDASDKASMKDAMSNKDA"
+API_KEY = "DASDASDKASMKDAMSNKDA"
+
 
 def soma(a, b):
-    return a+b
+    return a + b
+
 
 def subtracao(a, b):
     return a - b
 
+
 def multiplicacao(a, b):
     return a * b
 
+
 def divisao(a, b):
     return a / b
-
